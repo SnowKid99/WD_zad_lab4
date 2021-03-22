fbb2ba8bc21dea7df8dfda9237952bbd63148ce4
SnowKid99 <61504008+SnowKid99@users.noreply.github.com>
2021-03-22 08:21:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#zad4
+# class NaZakupy:
+#     def __init__(self,nazwa_produktu,ilosc,jednostka_miary,cena_jed):
+#         self.nazwa_produktu=nazwa_produktu
+#         self.ilosc=int(ilosc)
+#         self.jednostka_miary=jednostka_miary
+#         self.cena_jed=float(cena_jed)
+#     def wyswietl_produkt(self):
+#         print(self.nazwa_produktu)
+#         print(self.ilosc)
+#         print(self.jednostka_miary)
+#         print(self.cena_jed)
+#     def ile_produktu(self):
+#         print(str(self.ilosc)+str(self.jednostka_miary))
+#     def ile_kosztuje(self):
+#         cena=self.ilosc*self.cena_jed
+#         print(str(cena))
+
+#zad1
+# import sys
+# lista=[x for x in range(101) if x%4==0]
+# plik=open("liczby_podzielne_przez_4","w")
+# plik.writelines(str(lista))
+# plik.close()
+#
+# #zad2
+# plik=open("liczby_podzielne_przez_4","r")
+# print(plik.read())
+# plik.close()
+
